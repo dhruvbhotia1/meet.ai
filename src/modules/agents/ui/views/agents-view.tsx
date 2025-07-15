85a33b5eaa64e444ffb29661aa1d14d424c13229
bhotiadhruv1 <bhotiadhruv1@gmail.com>
2025-07-15 14:48:33 -0400

    added responsiveness to the search drawer

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,6 +5,8 @@ import {useSuspenseQuery} from "@tanstack/react-query"
 import {useTRPC} from "@/trpc/client";
 import LoadingState from "@/components/loading-state";
 import ErrorState from "@/components/error-state";
+import {ResponsiveDialog} from "@/components/responsive-dialog";
+import {Button} from "@/components/ui/button";
 
 const AgentsView = () => {
 
--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,5 +1,5 @@
 import React, {Dispatch, SetStateAction} from 'react';
-import {CommandDialog, CommandInput, CommandItem, CommandList} from "@/components/ui/command";
+import { CommandInput, CommandItem, CommandList, CommandResponsiveDialog} from "@/components/ui/command";
 
 interface Props {
     open : boolean,
@@ -8,7 +8,7 @@ interface Props {
 
 export const DashboardCommand = ({open, setOpen}: Props ) => {
     return (
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
             <CommandInput placeholder={'Find a meeting or agent'} />
             <CommandList>
                 <CommandItem>
@@ -16,7 +16,7 @@ export const DashboardCommand = ({open, setOpen}: Props ) => {
                 </CommandItem>
             </CommandList>
 
-        </CommandDialog>
+        </CommandResponsiveDialog>
     );
 };
 
